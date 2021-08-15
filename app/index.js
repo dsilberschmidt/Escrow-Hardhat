8cbbc79cc7963a264848bc301ddeb0a37b408fbd
Daniel Silberschmidt <daniel.silberschmidt@gmail.com>
2021-08-15 16:20:59 +0200

    trying to parse ether

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
-//import {ethers} from 'ethers';
+import {ethers} from 'ethers';
 //import ethers from 'ethers';
-import * as ethers from 'ethers';
+//import * as ethers from 'ethers
+
+//const { utils } = ethers;
 
 import deploy from './deploy';
 import addContract from './addContract';
@@ -10,10 +12,12 @@ let contracts = 0;
 async function newContract() {
   const beneficiary = document.getElementById("beneficiary").value;
   const arbiter = document.getElementById("arbiter").value;
-  const value = ethers.BigNumber.from(document.getElementById("ether").value);
+   const value = ethers.BigNumber.from(document.getElementById("ether").value);
 //  const value = ethers.BigNumber.from(document.getElementById("ether").value) /1e18 ;
 //  const value =  { BigNumber: "1000000000000000000" }
 //  const value = ethers.utils.parseEther.from(document.getElementById("ether").value);
+//  const value = utils.parseEther.from(document.getElementById("ether").value);
+
   console.log(value);
   const contract = await deploy(arbiter, beneficiary, value);
   addContract(++contracts, contract, arbiter, beneficiary, value);
